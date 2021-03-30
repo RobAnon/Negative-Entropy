8c54837ceef997a42c4e5cba89607abf549d482f
Rob Montgomery <ProFUNDity.Invest@gmail.com>
2021-03-30 11:02:57 -0400

    Fixed compiler errors

--- truffle/migrations/3_negative.js
+++ truffle/migrations/3_negative.js
@@ -1,6 +1,6 @@
 const NegativeEntropy = artifacts.require('NegativeEntropy');
 
 module.exports = async function (deployer) {
-  await deployer.deploy(NegativeEntropy);
+  await deployer.deploy(NegativeEntropy, '0x745ff5eCF724A55CE63e9bED61f774AdCD894A71');
   const erc721_2 = await NegativeEntropy.deployed();
 };
