14c909e532a33bf7f283035f13d7a56573513c48
jmsbrdfrd <58705345+jmsbrdfrd@users.noreply.github.com>
2021-04-05 12:59:38 +0100

    fade in 404

--- site/src/routes/PageNotExists.svelte
+++ site/src/routes/PageNotExists.svelte
@@ -4,7 +4,7 @@ import { exits } from "page";
 
 </script>
 <section>
-    <div class="error-404-container">
+    <div class="fade-in fade-in-1 error-404-container">
         <h1>Error <b>404</b>. Page doesn't exist...</h1>
     </div>
 </section>
