ee63aca0cced7a61b2831d0346c1011a2da2d4b3
RobAnon <ProFUNDity.Invest@gmail.com>
2021-04-06 14:07:44 -0400

    Changed server behavior to avoid CORS errors

--- server/api/index.js
+++ server/api/index.js
@@ -18,7 +18,9 @@ const ipfs = createClient('https://ipfs.infura.io:5001');
 app.use(express.json());
 app.use(cors());
 
-app.use(helmet());
+app.use(helmet(helmet.permittedCrossDomainPolicies({
+    permittedPolicies: "all",
+  })));
 app.use(fileupload({limits: { fileSize: 50 * 1024 * 1024 },}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
