66665f79f317b7f660a78a7c96ff7448e7cf5e06
RobAnon <ProFUNDity.Invest@gmail.com>
2021-04-05 20:00:33 -0400

    Deliberately introduced change

--- server/api/index.js
+++ server/api/index.js
@@ -17,6 +17,7 @@ const ipfs = createClient('https://ipfs.infura.io:5001');
 //Parse JSON 
 app.use(express.json());
 app.use(cors());
+
 app.use(helmet());
 app.use(fileupload({limits: { fileSize: 50 * 1024 * 1024 },}));
 app.use(express.json({limit: '50mb'}));
