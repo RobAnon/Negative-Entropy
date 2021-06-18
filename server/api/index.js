e8f1e74221c932d6f92b7a5b7166184da2f69419
RobAnon <ProFUNDity.Invest@gmail.com>
2021-06-18 13:21:41 -0400

    Revert "More server fixes 2"
    This reverts commit 6b65cb588303d742e660ce6b07d1d945c70e75b1.
    
    Undid

--- server/api/index.js
+++ server/api/index.js
@@ -58,6 +58,8 @@ app.use(fileupload({limits: { fileSize: 50 * 1024 * 1024 },}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
